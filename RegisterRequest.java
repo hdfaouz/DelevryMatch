ec4b6c73a3da82812e3217eef54f1bd4e60b2004
hdfaouz <hdfaouzia2@gmail.com>
2025-06-17 14:49:19 +0100

    RegisterRequest

--- RegisterRequest.java
+++ RegisterRequest.java
@@ -1,4 +1,16 @@
 package com.enaa.backend.Auth;
 
+import com.enaa.backend.Enum.Role;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
 public class RegisterRequest {
+    private String name;
+    private String email;
+    private String password;
+    private Role role;
 }
