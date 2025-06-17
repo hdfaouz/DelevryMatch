93abbff450d85b6f5aeb7ed521bd8a574a275e55
hdfaouz <hdfaouzia2@gmail.com>
2025-06-17 17:51:03 +0100

    Authentication

--- AuthenticationResponse.java
+++ AuthenticationResponse.java
@@ -1,4 +1,18 @@
 package com.enaa.backend.Auth;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AuthenticationResponse {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
